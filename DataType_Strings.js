6eb50516a6ae63e3559cbcc8af13f64464b65511
SFR <syahirfaturrahman231204@gmail.com>
2022-10-04 01:11:46 +0700

    Pushing all html and javascript files

---
+++ DataType_Strings.js
@@ -0,0 +1,3 @@
+console.log("First line");
+console.log("<hr/>");
+console.log("Second line");
---
+++ MathOperator_1.js
@@ -0,0 +1,47 @@
+/*
+    operasi hitung ke-1
+*/
+
+var a = 10;
+var b = 10;
+var c = a + b;
+console.log(c); //20
+
+/*
+    operasi hitung ke-2
+*/
+
+var d = 25;
+var e = 5;
+var f = d - e;
+console.log(f); //20
+
+/*
+    operasi hitung ke-3 :
+    Multiplication
+*/
+
+var g = 10;
+var h = 20;
+var i = g * h;
+console.log(i); //200
+
+/*
+    operasi hitung ke-4 : 
+    Division
+*/
+
+var j = 20;
+var k = 2;
+var l = j / k;
+console.log(l); //10
+
+/*
+    operasi hitung ke-5 : 
+    Modulus
+*/
+
+var m = 56;
+var n = 3;
+var o = m % n;
+console.log(o); //2
